bb82d10864c9c290d0428f02c640aba9da50f2ad
Kip <kip.vader@ya.ru>
2024-02-09 21:46:58 +0400

    add project model

---
+++ app/MoonShine/Pages/Project/ProjectFormPage.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Pages\Project;
+
+use MoonShine\Components\Boolean;
+use MoonShine\Decorations\Block;
+use MoonShine\Decorations\Tab;
+use MoonShine\Decorations\Tabs;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Text;
+use MoonShine\Pages\Crud\FormPage;
+
+class ProjectFormPage extends FormPage
+{
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                Tabs::make([
+                    Tab::make(__('moonshine::ui.resource.main_information'), [
+                        ID::make()
+                            ->sortable()
+                            ->showOnExport(),
+
+                        Text::make(__('moonshine::ui.project.title'), 'title')
+                            ->required()
+                            ->showOnExport(),
+
+                        Text::make(__('moonshine::ui.project.slug'), 'slug')
+                            ->required()
+                            ->showOnExport(),
+                    ])
+                ])
+            ]),
+        ];
+    }
+
+    protected function topLayer(): array
+    {
+        return [
+            ...parent::topLayer()
+        ];
+    }
+
+    protected function mainLayer(): array
+    {
+        return [
+            ...parent::mainLayer()
+        ];
+    }
+
+    protected function bottomLayer(): array
+    {
+        return [
+            ...parent::bottomLayer()
+        ];
+    }
+}
---
+++ app/MoonShine/Resources/ProjectResource.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use App\MoonShine\Pages\Project\ProjectIndexPage;
+use App\MoonShine\Pages\Project\ProjectFormPage;
+use App\MoonShine\Pages\Project\ProjectDetailPage;
+
+use MoonShine\Attributes\Icon;
+use MoonShine\Resources\ModelResource;
+
+/**
+ * @extends ModelResource<Project>
+ */
+#[Icon('heroicons.outline.rectangle-group')]
+
+class ProjectResource extends ModelResource
+{
+    protected string $model = Project::class;
+
+    protected string $title = 'Projects';
+
+    public function pages(): array
+    {
+        return [
+            ProjectIndexPage::make($this->title()),
+            ProjectFormPage::make(
+                $this->getItemID()
+                    ? __('moonshine::ui.edit')
+                    : __('moonshine::ui.add')
+            ),
+            ProjectDetailPage::make(__('moonshine::ui.show')),
+        ];
+    }
+
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+
+    public function search(): array
+    {
+        return ['name'];
+    }
+}
